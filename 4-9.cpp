823a78a2bb6b8a87f1041c50733922be21f9d7a9
keerthisri89 <20wh1a0439@bvrithyderabad.edu.in>
2022-12-16 12:56:44 +0530

    first commit

---
+++ 4-9.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n=10;
+	cout<<"  ";
+	for(int i=1;i<=n;i++){
+		cout<<"\t"<<i<<" ";
+	}
+	cout<<endl;
+	for(int k=1;k<=n;k++){
+		cout<<k<<" ";
+		for(int j=1;j<=n;j++){
+			cout<<"\t"<<k*j<<" ";
+	}
+	cout<<endl;
+		}
+	}
+
